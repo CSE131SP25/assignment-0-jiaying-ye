84ba92fcf8bbf37ba6e8861fe8f533a30bb9950d
julia <julia@172.27.16.163>
2025-01-17 15:48:06 -0600

    1.ver

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,25 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(60);
+		robot.turnLeft(30);
+		robot.forward(50);
+		robot.turnRight(30);
+		robot.forward(30);
+		robot.turnRight(180);
+		robot.forward(50);
+		robot.turnRight(30);
+		robot.forward(50);
+		robot.turnLeft(500);
+		robot.forward(50);
+		robot.turnRight(30);
+		robot.forward(50);
+		robot.turnLeft(500);
+		robot.forward(50);robot.turnRight(30);
+		robot.forward(50);
+		robot.turnLeft(500);
+		robot.forward(50);
+		
 	}
 
 }
